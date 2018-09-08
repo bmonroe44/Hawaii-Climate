db5d3197ce627e39a5d9f9e96f0112bdc4777724
Brian Monroe <brianmonroe77@gmail.com>
2018-09-07 22:53:30 -0500

    Flask update

--- climate.py
+++ climate.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 # Database Setup
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={'check_same_thread': False}, echo=True)
 
 # reflect an existing database into a new model
 Base = automap_base()
